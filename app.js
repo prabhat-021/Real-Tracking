7cd9581db89c31f1c81123918c1fd9bad60e8a7a
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-11-14 00:02:39 +0530

    Project setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //  WRITE A PROGRAM TO COPY TEXT FROM CLIPBOARd
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const socketio = require("socket.io");
+const http = require("http");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+    res.send("hey");
+});
+
+app.listen(3000, console.log("App is listening at port 3000"));
+
