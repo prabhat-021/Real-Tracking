b54f105f573990149194e0b80a2f1be68a87f747
prabhat-021 <prabhatsahrawat010203@gmail.com>
2024-11-14 12:20:16 +0530

    Completed

--- app.js
+++ app.js
@@ -12,9 +12,22 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-    res.send("hey");
+io.on("connection", function (socket) {
+    console.log("New WebSocket connection");
+
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
+        // console.log("Clipboard data received:", data);
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    })
 });
 
-app.listen(3000, console.log("App is listening at port 3000"));
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+server.listen(3000, console.log("App is listening at port 3000"));
 
